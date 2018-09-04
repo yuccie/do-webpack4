0796fef37ab71df235288d466cf63b22c580f5f4
yuccie <yuccie_du@163.com>
2018-09-04 23:08:31 +0800

    动态引入lodash，并单独打包生成自定义名字的chunk中

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 
-import { cube } from './math.js'
-import './style.css'
-import _ from 'lodash'
+// 现在不需要静态导入lodash了，而是通过动态导入来分离一个chunk
 
-console.log(
-	_.join(['生产', 'module', 'loaded!'], ' ')
-)
-
-if(process.env.NODE_ENV === 'production'){
-	console.log('现在是生产模式');
-}
-function component(){
-	var ele = document.createElement('pre')
-	ele.innerHTML = `
-		hello webpack!
-		5 cubed is equal to ${cube(5)}
-	`
-	return ele
+function getComponent(){
+	// 下面注释部分使用webpackChunkName，这样做会导致我们的bundle被命名为lodash.bundle.js
+	// 而不是[id].bundle.js
+	return import(/* webpackChunkName: "lodash" */ 'lodash').then(
+		_ => {
+			var element = document.createElement('div')
+			element.innetHTML = _.join(['hello','webpack4'],' ')
+			return element
+		}
+	).catch(err => 'An error occurred while loading the component')
 }
 
-document.body.appendChild(component())
+getComponent().then(component => {
+	document.body.appendChild(component)
+})
--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -5,7 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
 	entry:{
 		app:'./src/index.js',
-		another:'./src/another-module.js'
 	},
 	plugins:[
 		new CleanWebpackPlugin(['dist']),
@@ -26,6 +25,7 @@ module.exports = {
 	},
 	output:{
 		filename:'[name].bundle.js',
+		chunkFilename:'[name].bundle.js',//决定非入口chunk的名称
 		path:path.resolve(__dirname,'dist')
 	}
 }
