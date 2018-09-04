c2965c06ede6b2c5c4f7b6413cba1e216c32966c
dujichong <dujichong@finupgroup.com>
2018-09-04 20:00:55 +0800

    生产环境开启source-map

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,8 +3,12 @@ const webpackCommonConfig = require('./webpack.common')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = webpackMerge(webpackCommonConfig,{
+	devtool:'source-map',
 	mode:'production',
 	plugins:[
-		new UglifyJsPlugin()
+		new UglifyJsPlugin({
+			// 避免在生产中使用 inline-*** 和 eval-***，因为它们可以增加 bundle 大小，并降低整体性能。
+			sourceMap:true
+		})
 	]
 })
