8afdd4e5b22bc5fffcf45541241865526c7e26e3
dujichong <dujichong@finupgroup.com>
2018-09-04 17:56:37 +0800

    HMR模式下修改样式表

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import printMe from './print.js'
+import './style.css'
 
 function component(){
 	var ele = document.createElement('div')
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
         contentBase:'./dist',
         hot:true
     },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    },
     plugins:[
         // 虽然output里的名字根据entry的名字动态变化，但index.html里的引入名字还得手动改，因此引入html-webpack-plugin插件，这个插件自动生成index.html文件，并注入响应的各种外链资源
         new HtmlWebpackPlugin({
