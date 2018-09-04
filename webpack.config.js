38235cd134c4f510ea3947328b71edc6f004bce3
dujichong <dujichong@finupgroup.com>
2018-09-04 16:28:04 +0800

    引入html-weppack-plugin插件，自动更新index.html，并更新外链文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
 
 module.exports = {
     entry:{
         app:'./src/index.js',
         print:'./src/print.js'
     },
+    plugins:[
+        // 虽然output里的名字根据entry的名字动态变化，但index.html里的引入名字还得手动改，因此引入html-webpack-plugin插件，这个插件自动生成index.html文件，并注入响应的各种外链资源
+        new HtmlWebpackPlugin({
+            title:'output Management 输出管理'
+        })
+    ],
     output:{
         filename:'[name].boundle.js',
         path:path.resolve(__dirname,'dist')
